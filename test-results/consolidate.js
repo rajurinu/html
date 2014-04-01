2ad619a9e0bfe2335b45c0826621f5d39431ef73
Robin Berjon <robin@berjon.com>
2014-04-01 12:49:24 +0200

    first <2 report

--- test-results/consolidate.js
+++ test-results/consolidate.js
@@ -31,11 +31,14 @@ for (var ua in data) {
         if (!out.results[id].totals[testData.status]) out.results[id].totals[testData.status] = 1;
         else out.results[id].totals[testData.status]++;
         for (var j = 0, m = testData.subtests.length; j < m; j++) {
-            var st = testData.subtests[j];
-            if (!out.results[id].subtests[st.name]) out.results[id].subtests[st.name] = { byUA: {}, totals: {} };
-            out.results[id].subtests[st.name].byUA[ua] = st.status;
-            if (!out.results[id].subtests[st.name].totals[st.status]) out.results[id].subtests[st.name].totals[st.status] = 1;
-            else out.results[id].subtests[st.name].totals[st.status]++;
+            var st = testData.subtests[j]
+            ,   nm = st.name
+            ;
+            if (nm === "constructor") nm = "_constructor";
+            if (!out.results[id].subtests[nm]) out.results[id].subtests[nm] = { byUA: {}, totals: {} };
+            out.results[id].subtests[nm].byUA[ua] = st.status;
+            if (!out.results[id].subtests[nm].totals[st.status]) out.results[id].subtests[nm].totals[st.status] = 1;
+            else out.results[id].subtests[nm].totals[st.status]++;
         }
     }
 }
